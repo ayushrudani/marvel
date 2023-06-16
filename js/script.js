7c3dfd1ad0a52dc440a4d05f679502a75776a95c
Ayush Rudani <ayushrudani09@gmail.com>
2023-06-16 13:31:48 +0530

    a

--- js/script.js
+++ js/script.js
@@ -11,3 +11,15 @@ var swiper = new Swiper(".home", {
     },
 
 });
+
+var moviecontauiner = document.getElementById("movies-container");
+for (i = 1; i < 11; i++) {
+    moviecontauiner.innerHTML += `<!-- Box-${i} -->
+    <div class="box">
+        <div class="box-img">
+            <img src="./assets/img/m${i}.jpg" alt="">
+        </div>
+        <h3>Venom</h3>
+        <span>120 min | Action</span>
+    </div>`
+}
